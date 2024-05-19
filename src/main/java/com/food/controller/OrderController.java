e1c253dd74d58b0117df97e019b893fb0f8bbe71
shivamgouri <shivamgouri1997@gmail.com>
2024-05-19 18:08:06 +0530

    feat: added user-changes

--- src/main/java/com/food/controller/OrderController.java
+++ src/main/java/com/food/controller/OrderController.java
@@ -1,24 +1,37 @@
 package com.food.controller;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.food.model.Order;
 import com.food.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
+@RequestMapping("/orders")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
 
-    @PostMapping("/orders")
-    public ResponseEntity<Order> createOrder(@RequestBody Order order) throws JsonProcessingException {
+    @PostMapping("/create")
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        orderService.createOrder(order);
+        return ResponseEntity.ok(order);
+    }
+
+//    @GetMapping("/create")
+//    public ResponseEntity<String> createOrder() {
+//        return ResponseEntity.ok("abx");
+//    }
+
+
+    @PostMapping("/cancel")
+    public ResponseEntity<Order> cancelOrder(@RequestBody Order order) {
         orderService.createOrder(order);
         return ResponseEntity.ok(order);
     }
